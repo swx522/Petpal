11388fc0f71a6a854fcc9930ceca817a9ff59c83
Catcher-01 <2443589503@qq.com>
2026-01-02 15:15:11 +0800

    更新表

---
+++ Backend/Migrations/20260102070844_AddLocationFieldsToMutualOrders.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace petpal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocationFieldsToMutualOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latitude",
+                table: "MutualOrders",
+                type: "decimal(65,30)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Longitude",
+                table: "MutualOrders",
+                type: "decimal(65,30)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "MutualOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "MutualOrders");
+        }
+    }
+}
